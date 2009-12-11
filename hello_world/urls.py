64501e886c4440155a84e7ab53c70045472eab25
David Ignacio <dignacio@litl.com>
2009-12-11 18:41:46 -0500

    alternate versioning using the decorator

--- hello_world/urls.py
+++ hello_world/urls.py
@@ -5,4 +5,5 @@ from django.conf.urls.defaults import include, patterns
 urlpatterns = patterns('hello_world.views',
                        (r'timestamp', 'timestamp'),
                        (r'utctimestamp', 'utctimestamp'),
+                       (r'version', 'version'),
                        (r'', 'hello'))
--- hello_world/views.py
+++ hello_world/views.py
@@ -4,18 +4,19 @@ import datetime
 
 from django.http import HttpResponse
 
-from hello_world.versioning import is_api_version, versioned_method
+from hello_world.versioning import get_api_version, is_api_version, versioned_method
 
 def hello(request):
     return HttpResponse('hello world')
 
-def timestamp(request):
-    if is_api_version(request, '1'):
-        now = datetime.datetime.utcnow()
-    else:
-        now = datetime.datetime.now()
-    return HttpResponse(now.isoformat())
-
 def utctimestamp(request):
     now = datetime.datetime.utcnow()
     return HttpResponse(now.isoformat())
+
+@versioned_method(versions={ '1':utctimestamp })
+def timestamp(request):
+    now = datetime.datetime.now()
+    return HttpResponse(now.isoformat())
+
+def version(request):
+    return HttpResponse(get_api_version(request))
