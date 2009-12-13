81a8eafe33f8db4a0f0f1cc3592e7f1c08bd8aa1
David Ignacio <deignacio@gmail.com>
2009-12-13 18:30:53 -0500

    add v2 timestamp that returns data in JSON

--- hello_world/views.py
+++ hello_world/views.py
@@ -3,6 +3,7 @@
 import datetime
 
 from django.http import HttpResponse
+from django.utils import simplejson
 
 from hello_world.versioning import get_api_version, is_api_version, versioned_method
 
@@ -11,7 +12,12 @@ def hello(request):
 
 def utctimestamp(request):
     now = datetime.datetime.utcnow()
-    return HttpResponse(now.isoformat())
+    if is_api_version(request, '2'):
+        data = { "now": now.isoformat() }
+        json = simplejson.dumps(data)
+        return HttpResponse(json)
+    else:
+        return HttpResponse(now.isoformat())
 
 @versioned_method(versions={ '1':utctimestamp })
 def timestamp(request):
