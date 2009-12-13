9af88a1497e7da31f06e909b02487a4f02767069
David Ignacio <deignacio@gmail.com>
2009-12-13 18:33:30 -0500

    use a few decorators to refactor out a bunch of common code

--- hello_world/views.py
+++ hello_world/views.py
@@ -7,22 +7,46 @@ from django.utils import simplejson
 
 from hello_world.versioning import get_api_version, is_api_version, versioned_method
 
-def hello(request):
-    return HttpResponse('hello world')
+def to_json(func):
+    def inner_to_json(request, *args, **kwargs):
+        obj = func(request, *args, **kwargs)
+        json = simplejson.dumps(obj)
+        return json
+    return inner_to_json
 
+def to_http_response(response_type=None):
+    if response_type is None:
+        response_type = HttpResponse
+    def inner_return_to_http_response(func):
+        def actual_return_to_http_response(request, *args, **kwargs):
+            val = func(request, *args, **kwargs)
+            return response_type(val)
+        return actual_return_to_http_response
+    return inner_return_to_http_response
+
+@to_http_response()
+def hello(request):
+    return 'hello world'
+
+@to_http_response()
 def utctimestamp(request):
     now = datetime.datetime.utcnow()
-    if is_api_version(request, '2'):
-        data = { "now": now.isoformat() }
-        json = simplejson.dumps(data)
-        return HttpResponse(json)
-    else:
-        return HttpResponse(now.isoformat())
+    return now.isoformat()
 
-@versioned_method(versions={ '1':utctimestamp })
+@to_http_response()
+@to_json
+def utctimestamp_json(request):
+    now = datetime.datetime.utcnow()
+    data = { "now": now.isoformat() }
+    return data
+
+@versioned_method(versions={ '1':utctimestamp,
+                             '2':utctimestamp_json })
+@to_http_response()
 def timestamp(request):
     now = datetime.datetime.now()
-    return HttpResponse(now.isoformat())
+    return now.isoformat()
 
+@to_http_response()
 def version(request):
-    return HttpResponse(get_api_version(request))
+    return get_api_version(request)
